30ffcb91b2f458932b9a18233ae868e649572696
stef427 <stef_427@hotmail.com>
2020-09-24 16:04:32 +0200

    second

--- script.js
+++ script.js
@@ -22,8 +22,11 @@ function calculAge() {
     console.log(days);
     let years = parseInt(days / 365.25);
     console.log(years);
-
-    if (anniversaire) {
+    if (anniversaire && years <= 1) {
+        alert("joyeux anniversaire petit " + firstName + " " + name + "pour vos" + years + "an!!!");
+    } else if (anniversaire) { //si on met un point ! c 'est si on veux l'inverse
         alert("joyeux Anniversaire " + firstName + " " + name + " pour vos " + years + " ans!!");
+    } else {
+        alert("vous avez" + years + "ans");
     }
 }
